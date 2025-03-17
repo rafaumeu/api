2c1b71f5751d1269e74fadaa683c5c587446e3fa
maycorolbuche <mayco_rolbuche@hotmail.com>
2025-03-17 12:07:55 -0300

    Ajustes

--- app/Helpers/OnlineVideos.php
+++ app/Helpers/OnlineVideos.php
@@ -12,10 +12,26 @@ class OnlineVideos
     {
         $youtube = new YoutubeService();
 
-        $channels = OnlineVideoChannel::where('name', '')->orWhereNull('name')->get();
+        $channels = OnlineVideoChannel::where('status', 'pending')->get();
         foreach ($channels as $channel) {
-            dd($youtube->channel($channel->id_youtube));
-            print_r($channel->toArray());
+            $data = $youtube->channel($channel->channel_id);
+            if (isset($data["error"])) {
+                $channel->error = $data["error"];
+            } else {
+                $channel->error = null;
+                $channel->name = $data["snippet"]["title"];
+                $channel->description = $data["snippet"]["description"];
+                $channel->custom_url = $data["snippet"]["customUrl"];
+                $channel->default_image = $data["snippet"]["thumbnails"]["default"]["url"];
+                $channel->medium_image = $data["snippet"]["thumbnails"]["medium"]["url"];
+                $channel->high_image = $data["snippet"]["thumbnails"]["high"]["url"];
+                $image_data = file_get_contents($data["snippet"]["thumbnails"]["default"]["url"]);
+                //dd($data["snippet"]["thumbnails"]["default"]["url"], $image_data);
+                $channel->default_image_base64 = 'data:image/' . explode('/', mime_content_type($data["snippet"]["thumbnails"]["default"]["url"]))[1] . ';base64,' . base64_encode($image_data);
+            }
+            $channel->save();
+            //            $channel->description = $youtube->channel($channel->channel_id)['snippet']['description'];
+
         }
         return ["logs" => "OLA MUNDO"];
     }
