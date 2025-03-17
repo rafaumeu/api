4beb2e8781a777ebda3df84226f41a72074fe18f
maycorolbuche <mayco_rolbuche@hotmail.com>
2025-03-17 19:24:27 -0300

    Database

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -100,7 +100,7 @@ class Kernel extends ConsoleKernel
                 $telegramService->sendMessage("⏰ Rotina executada: Exportação de Banco de Dados!");
                 $telegramService->sendMessage("<pre>" . json_encode($ret, JSON_PRETTY_PRINT) . "</pre>");
             }
-        })->daily();
+        })->hourly();
 
         $schedule->call(function () {
             $controller = new TaskController();
