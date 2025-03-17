8278ff86cff2dea085c20a2e535099aca8d5ed49
maycorolbuche <mayco_rolbuche@hotmail.com>
2025-03-17 08:12:06 -0300

    Migration

--- database/migrations/2025_03_13_185423_create_ftp_logs.php
+++ database/migrations/2025_03_13_185423_create_ftp_logs.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class FtpLogs extends Migration
+class CreateFtpLogsTable extends Migration
 {
     /**
      * Run the migrations.
