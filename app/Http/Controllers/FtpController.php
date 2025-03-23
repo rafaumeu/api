d4b169896d2f9c6b222654410bf8f6548743b754
maycorolbuche <mayco_rolbuche@hotmail.com>
2025-03-23 12:18:28 -0300

    Ajuste

--- app/Http/Controllers/FtpController.php
+++ app/Http/Controllers/FtpController.php
@@ -13,10 +13,6 @@ class FtpController extends Controller
     public function index(Request $request)
     {
         $id_language = strtolower($request->id_language ?? $request->query('lang') ?? "pt");
-        if ($request->data) {
-            parse_str(base64_decode($request->data), $p);
-            $id_language = strtolower($p["lang"] ?? $id_language) ?: $id_language;
-        }
 
         //REMOVER DEPOIS -- RETROCOMPATIBILIDADE
         if (!$request->get("token")) {
