40582cbf1a49fa6e894e802c0d13c668047a63d1
maycorolbuche <mayco_rolbuche@hotmail.com>
2025-11-12 12:14:56 -0300

    Ajsute Player

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -39,19 +39,15 @@ class PlayerController extends Controller
                 </style>
             </head>
             <body style='border:0'>
-                <!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
                 <div id='player'></div>
 
                 <script>
-                // 2. This code loads the IFrame Player API code asynchronously.
                 var tag = document.createElement('script');
 
                 tag.src = 'https://www.youtube.com/iframe_api';
                 var firstScriptTag = document.getElementsByTagName('script')[0];
                 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-                // 3. This function creates an <iframe> (and YouTube player)
-                //    after the API code downloads.
                 var player;
                 function onYouTubeIframeAPIReady() {
                     player = new YT.Player('player', {
