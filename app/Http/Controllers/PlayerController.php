30277ee01e879563d744398e152d46f559091a2c
maycorolbuche <mayco_rolbuche@hotmail.com>
2025-11-12 11:06:53 -0300

    Ajuste Player

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -20,8 +20,8 @@ class PlayerController extends Controller
                     <style>
                         iframe{
                             position:absolute;
-                            width:100vw;
-                            height:100vh;
+                            width:100%;
+                            height:100%;
                             top:0;
                             left:0;
                         }
